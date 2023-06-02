02eeb4c6e7fabe03448f12da357a4d8bcca4590a
Akshit <akshit.khandelwal@dataweave.com>
2023-06-02 23:31:13 +0530

    created two tables

--- restaurants/restaurants_info/models.py
+++ restaurants/restaurants_info/models.py
@@ -6,3 +6,13 @@ class storestatus(models.Model):
     store_id = models.IntegerField()
     status = models.CharField(max_length=20)
     timestamp = models.DateTimeField()
+
+class TimeZone(models.Model):
+    store_id = models.IntegerField()
+    timezone_str = models.CharField(max_length=30)
+
+class MenuHours(models.Model):
+    store_id = models.IntegerField()
+    week_day = models.IntegerField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
